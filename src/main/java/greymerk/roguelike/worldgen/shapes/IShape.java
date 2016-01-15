cad7c92991fbf523e892e86abed291f54ab8939f
Brian Rivas <brian.rivas64@gmail.com>
2016-01-15 01:53:26 -0800

    Added shape interface and classes

---
+++ src/main/java/greymerk/roguelike/worldgen/shapes/IShape.java
@@ -0,0 +1,15 @@
+package greymerk.roguelike.worldgen.shapes;
+
+import java.util.Iterator;
+import java.util.Random;
+
+import greymerk.roguelike.worldgen.Coord;
+import greymerk.roguelike.worldgen.IBlockFactory;
+import greymerk.roguelike.worldgen.WorldEditor;
+
+public interface IShape{
+
+	public void fill(WorldEditor editor, Random rand, IBlockFactory block, boolean fillAir, boolean replaceSolid);
+	
+	public Iterator<Coord> iter();
+}
---
+++ src/main/java/greymerk/roguelike/worldgen/shapes/RectSolid.java
@@ -0,0 +1,41 @@
+package greymerk.roguelike.worldgen.shapes;
+
+import java.util.Iterator;
+import java.util.Random;
+
+import greymerk.roguelike.worldgen.Coord;
+import greymerk.roguelike.worldgen.IBlockFactory;
+import greymerk.roguelike.worldgen.WorldEditor;
+
+public class RectSolid implements IShape {
+
+	private Coord start;
+	private Coord end;
+	
+	public RectSolid(Coord start, Coord end){
+		this.start = start;
+		this.end = end;
+	}
+	
+	@Override
+	public void fill(WorldEditor editor, Random rand, IBlockFactory block, boolean fillAir, boolean replaceSolid) {
+		Coord c1 = new Coord(start);
+		Coord c2 = new Coord(end);
+		
+		Coord.correct(c1, c2);
+		
+		for(int x = c1.getX(); x <= c2.getX(); x++){
+			for(int y = c1.getY(); y <= c2.getY(); y++){
+				for(int z = c1.getZ(); z <= c2.getZ(); z++){
+					editor.setBlock(rand, new Coord(x, y, z), block, fillAir, replaceSolid);
+				}
+			}
+		}
+	}
+
+	@Override
+	public Iterator<Coord> iter() {
+		return null;
+	}
+
+}
