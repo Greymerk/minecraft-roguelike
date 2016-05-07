d6cd9026815a5915878212f8b284d2fa2d2542ef
Brian Rivas <brian.rivas64@gmail.com>
2016-05-06 17:53:51 -0700

    version update

--- src/main/java/greymerk/roguelike/Roguelike.java
+++ src/main/java/greymerk/roguelike/Roguelike.java
@@ -21,7 +21,7 @@ public class Roguelike {
 	// The instance of your mod that Forge uses.
 	@Instance("Roguelike")
 	public static Roguelike instance;
-	public static final String version = "1.5.0";
+	public static final String version = "1.5.1";
 	
 	// Says where the client and server 'proxy' code is loaded.
 	@SidedProxy(clientSide="greymerk.roguelike.ClientProxy", serverSide="greymerk.roguelike.CommonProxy")
