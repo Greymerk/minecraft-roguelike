538665b55b07743208fca1479c5be26f26199c57
Brian Rivas <brian.rivas64@gmail.com>
2016-11-25 10:22:38 -0800

    Added vanilla Spawn Potentials to spawners

--- src/main/java/greymerk/roguelike/treasure/loot/Equipment.java
+++ src/main/java/greymerk/roguelike/treasure/loot/Equipment.java
@@ -1,7 +1,53 @@
 package greymerk.roguelike.treasure.loot;
 
+import java.util.Arrays;
+
 public enum Equipment {
 	
 	SWORD, BOW, HELMET, CHEST, LEGS, FEET, PICK, AXE, SHOVEL;
 
+	public static Equipment[] armour = new Equipment[]{HELMET, CHEST, LEGS, FEET};
+	
+	public static String getName(Equipment type, Quality quality){
+		
+		String qualityName;
+		String itemName;
+		
+		switch(type){
+		case SWORD: itemName = "sword"; break;
+		case BOW: return "minecraft:bow";
+		case HELMET:itemName = "helmet"; break;
+		case CHEST:itemName = "chestplate"; break;
+		case LEGS: itemName = "leggings"; break;
+		case FEET: itemName = "boots"; break;
+		case PICK: itemName = "pickaxe"; break;
+		case AXE: itemName = "axe"; break;
+		case SHOVEL: itemName = "shovel"; break;
+		default: return "minecraft:stick";
+		}
+		
+		if(Arrays.asList(Equipment.armour).contains(type)){
+			switch(quality){
+			case WOOD: qualityName = "leather"; break;
+			case STONE: qualityName = "chainmail"; break;
+			case IRON: qualityName = "iron"; break;
+			case GOLD: qualityName = "golden"; break;
+			case DIAMOND: qualityName = "diamond"; break;
+			default: return "minecraft:stick";
+			}
+		} else {
+			switch(quality){
+			case WOOD: qualityName = "wooden"; break;
+			case STONE: qualityName = "stone"; break;
+			case IRON: qualityName = "iron"; break;
+			case GOLD: qualityName = "golden"; break;
+			case DIAMOND: qualityName = "diamond"; break;
+			default: return "minecraft:stick";
+			}
+		}
+		
+
+		
+		return "minecraft:" + qualityName + "_" + itemName;
+	}
 }
--- src/main/java/greymerk/roguelike/worldgen/spawners/Spawnable.java
+++ src/main/java/greymerk/roguelike/worldgen/spawners/Spawnable.java
@@ -5,6 +5,8 @@ import java.util.Random;
 import com.google.gson.JsonObject;
 
 import greymerk.roguelike.config.RogueConfig;
+import greymerk.roguelike.treasure.loot.Equipment;
+import greymerk.roguelike.treasure.loot.Quality;
 import greymerk.roguelike.util.JsonNBT;
 import greymerk.roguelike.worldgen.Coord;
 import greymerk.roguelike.worldgen.IWorldEditor;
@@ -80,11 +82,16 @@ public class Spawnable {
             return;
         }
         
-        NBTTagCompound nbt = getRoguelike(level, this.name);
+        NBTTagCompound nbt = new NBTTagCompound();
+        NBTTagList potentials = getSpawnPotentials(rand, level, this.name);
+
+    	NBTTagCompound spawn = potentials.getCompoundTagAt(0);
+    	nbt.setTag("SpawnData", spawn.getTag("Entity"));        
+       	nbt.setTag("SpawnPotentials", potentials);	
+        
         nbt.setInteger("x", pos.getX());
         nbt.setInteger("y", pos.getY());
         nbt.setInteger("z", pos.getZ());
-        
         
         spawnerLogic.readFromNBT(nbt);
         spawnerLogic.updateSpawner();
@@ -92,21 +99,94 @@ public class Spawnable {
         
 	}
 	
-	private NBTTagCompound getRoguelike(int level, String type){
+	private NBTTagList getSpawnPotentials(Random rand, int level, String name){
+		
+		NBTTagList potentials = new NBTTagList();
+		
+		if(name.equals(Spawner.getName(Spawner.ZOMBIE))){
+			for(int i = 0; i < 24; ++i){
+				NBTTagCompound mob = new NBTTagCompound();
+				mob = getRoguelike(level, this.name, mob);
+				
+				Equipment tool;
+				switch(rand.nextInt(3)){
+				case 0: tool = Equipment.SHOVEL; break;
+				case 1: tool = Equipment.AXE; break;
+				case 2: tool = Equipment.PICK; break;
+				default: tool = Equipment.PICK; break;
+				}
+				
+				mob = equipHands(mob, Equipment.getName(tool, Quality.getToolQuality(rand, level)), "minecraft:shield");
+				mob = equipArmour(mob, rand, level);
+				
+				potentials.appendTag(getPotential(mob));
+			}
+			
+			return potentials;
+		}
+		
+		if(name.equals(Spawner.getName(Spawner.SKELETON))){
+			for(int i = 0; i < 12; ++i){
+				NBTTagCompound mob = new NBTTagCompound();
+				mob = getRoguelike(level, this.name, mob);
+				mob = equipHands(mob, "minecraft:bow", null);
+				mob = equipArmour(mob, rand, level);
+				potentials.appendTag(getPotential(mob));
+			}
+			
+			return potentials;
+		}
+
+		potentials.appendTag(getPotential(getRoguelike(level, this.name, new NBTTagCompound())));
+		return potentials;
+	}
+
+	private NBTTagCompound getPotential(NBTTagCompound mob){
+		NBTTagCompound potential = new NBTTagCompound();
+		potential.setTag("Entity", mob);
+		potential.setInteger("Weight", 1);
+		return potential;
+	}
+
+	private NBTTagCompound equipHands(NBTTagCompound mob, String weapon, String offhand){
+		NBTTagList hands = new NBTTagList();
+		hands.appendTag(getItem(weapon));
+		hands.appendTag(getItem(offhand));
+		mob.setTag("HandItems", hands);
+		
+		return mob;
+	}
+	
+	private NBTTagCompound equipArmour(NBTTagCompound mob, Random rand, int level){
+		
+		NBTTagList armour = new NBTTagList();
+		armour.appendTag(getItem(Equipment.getName(Equipment.FEET, Quality.getArmourQuality(rand, level))));
+		armour.appendTag(getItem(Equipment.getName(Equipment.LEGS, Quality.getArmourQuality(rand, level))));
+		armour.appendTag(getItem(Equipment.getName(Equipment.CHEST, Quality.getArmourQuality(rand, level))));
+		armour.appendTag(getItem(Equipment.getName(Equipment.HELMET, Quality.getArmourQuality(rand, level))));
+		mob.setTag("ArmorItems", armour);
+
+		return mob;
+	}
+	
+	private NBTTagCompound getItem(String name){
+		NBTTagCompound item = new NBTTagCompound();
+		if(name == null) return item;
+		item.setString("id", name);
+		item.setInteger("Count", 1);
+		return item;
+	}
+	
+	private NBTTagCompound getRoguelike(int level, String type, NBTTagCompound tag){
 		//String strg = "{SpawnData:{id:\"" + type + "\",ActiveEffects:[{Id:4,Duration:10,Amplifier:" + level + ",Ambient:0}]}}";
     	
-		NBTTagCompound nbt = new NBTTagCompound();
-		
-		NBTTagCompound spawnData = new NBTTagCompound();
-		nbt.setTag("SpawnData", spawnData);
-		
-		spawnData.setString("id", type);
+		tag.setString("id", type);
 		
         if(!(RogueConfig.getBoolean(RogueConfig.ROGUESPAWNERS)
-        		&& this.equip)) return nbt;
+        		&& this.equip)) return tag;
 		
 		NBTTagList activeEffects = new NBTTagList();
-		spawnData.setTag("ActiveEffects", activeEffects);
+		tag.setTag("ActiveEffects", activeEffects);
 		
 		NBTTagCompound buff = new NBTTagCompound();
 		activeEffects.appendTag(buff);
@@ -116,6 +196,8 @@ public class Spawnable {
 		buff.setInteger("Duration", 10);
 		buff.setByte("Ambient", (byte) 0);
 
-		return nbt;
+		return tag;
     }
+	
+
 }
