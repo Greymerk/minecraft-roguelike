4e22940ddc2bf8493db0b795a50cc1d0f2b2dccf
Brian Rivas <brian.rivas64@gmail.com>
2017-10-29 11:16:25 -0700

    implemented tree generator

--- src/main/java/greymerk/roguelike/dungeon/towers/TreeTower.java
+++ src/main/java/greymerk/roguelike/dungeon/towers/TreeTower.java
@@ -1,33 +1,37 @@
 package greymerk.roguelike.dungeon.towers;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import greymerk.roguelike.theme.ITheme;
-import greymerk.roguelike.util.DyeColor;
+import greymerk.roguelike.worldgen.BlockJumble;
 import greymerk.roguelike.worldgen.Cardinal;
 import greymerk.roguelike.worldgen.Coord;
-import greymerk.roguelike.worldgen.IBlockFactory;
 import greymerk.roguelike.worldgen.IWorldEditor;
+import greymerk.roguelike.worldgen.MetaBlock;
 import greymerk.roguelike.worldgen.blocks.BlockType;
-import greymerk.roguelike.worldgen.blocks.ColorBlock;
+import greymerk.roguelike.worldgen.blocks.Leaves;
 import greymerk.roguelike.worldgen.blocks.Log;
 import greymerk.roguelike.worldgen.blocks.Wood;
 import greymerk.roguelike.worldgen.shapes.Line;
 import greymerk.roguelike.worldgen.shapes.RectSolid;
+import greymerk.roguelike.worldgen.shapes.Sphere;
 
 public class TreeTower implements ITower{
 
 	@Override
 	public void generate(IWorldEditor editor, Random rand, ITheme theme, Coord pos) {
 		
+		Coord start;
+		Coord end;
 		Coord ground = Tower.getBaseCoord(editor, pos);
 		
-		Coord start = new Coord(ground);
-		start.add(new Coord(-4, -1, -4));
-		Coord end = new Coord(ground);
-		end.add(new Coord(4, 10, 4));
+		start = new Coord(ground);
+		start.add(Cardinal.DOWN, 10);
 		
-		//Log.getLog(Wood.OAK).fill(editor, rand, new RectSolid(start, end));
+
+		new Branch(rand, start).generate(editor, rand);
 		
 		start = new Coord(ground);
 		start.add(new Coord(-2, 0, -2));
@@ -35,21 +39,117 @@ public class TreeTower implements ITower{
 		end.add(new Coord(2, 5, 2));
 		BlockType.get(BlockType.AIR).fill(editor, rand, new RectSolid(start, end));
 		
-		for(Coord p : new RectSolid(ground, pos)){
+		start = new Coord(ground);
+		start.add(Cardinal.DOWN);
+		for(Coord p : new RectSolid(start, pos)){
 			editor.spiralStairStep(rand, p, theme.getPrimary().getStair(), Log.getLog(Wood.OAK));
 		}
+	}
+	
+	private class Branch{
 		
-		start = new Coord(ground);
-		start.add(Cardinal.UP, 40);
-		end = new Coord(start);
-		start.add(new Coord(-30, 0, -30));
-		end.add(new Coord(30, 0, 30));
+		Coord start;
+		Coord end;
+		List<Branch> branches;
+		int thickness;
 		
-		for(Coord c : new RectSolid(start, end)){
-			if(c.getX() % 5 != 0 || c.getZ() % 5 != 0) continue;	
-			IBlockFactory glass = ColorBlock.get(ColorBlock.GLASS, DyeColor.values()[rand.nextInt(DyeColor.values().length)]);
-			new Line(ground, c).fill(editor, rand, glass);	
+		public Branch(Random rand, Coord start){
+			this.start = new Coord(start);
+			this.branches = new ArrayList<Branch>();
+			int counter = 12;
+			int thickness = 5;
+			this.thickness = thickness;
+			int density = 3;
+			double noise = 0.3;
+			double pitch = 0;
+			double yaw = Math.PI / 2;
+			this.end = getEnd(start, 4, pitch, yaw);
+			
+			for(int i = 0; i < density; ++i){
+				this.branches.add(
+					new Branch(
+						rand,
+						new Coord(end),
+						counter,
+						10,
+						thickness,
+						3,
+						noise,
+						pitch + ((Math.PI * 2 / density) * i),
+						yaw + (rand.nextDouble() - 0.5) * noise
+					)
+				);
+			}
 		}
 		
+		public Branch(Random rand, Coord start, int counter, int length, int thickness, int density, double noise, double pitch, double yaw){
+			
+			this.start = new Coord(start);
+			this.thickness = thickness;
+			this.branches = new ArrayList<Branch>();
+			this.end = getEnd(start, length, pitch, yaw);
+			
+			if(counter <= 0 || thickness <= 0 || length <= 0) return;
+			
+			for(int i = 0; i < density; ++i){
+				this.branches.add(
+					new Branch(
+						rand,
+						new Coord(end),
+						counter - 1,
+						length - 1,
+						thickness - 1,
+						density + 1,
+						noise + 0.3,
+						pitch + (rand.nextDouble() - 0.5) * noise,
+						yaw + (rand.nextDouble() - 0.5) * noise
+					)
+				);
+			}
+		}
+		
+		public void generate(IWorldEditor editor, Random rand){
+			
+			MetaBlock oak = Log.get(Wood.OAK, start.dirTo(end));
+			
+			for(Branch b : this.branches){
+				b.generate(editor, rand);
+			}
+			
+			if(thickness == 1){
+				new Line(start, end).fill(editor, rand, oak);
+			} else if(thickness > 1){
+				for(Coord pos : new Line(start, end)){
+					Coord s = new Coord(pos);
+					Coord e = new Coord(s);
+					e.add(new Coord(thickness, thickness, thickness));
+					new Sphere(s, e).fill(editor, rand, oak, true, false);
+				}
+			}
+			
+			if(this.branches.isEmpty() && rand.nextInt(3) == 0){
+				BlockJumble leaves = new BlockJumble();
+				leaves.addBlock(Leaves.get(Leaves.OAK, false));
+				leaves.addBlock(BlockType.get(BlockType.AIR));
+				
+				Coord s = new Coord(end);
+				Coord e = new Coord(s);
+				e.add(new Coord(3, 3, 3));
+				new Sphere(s, e).fill(editor, rand, leaves, true, false);
+			}
+		}
+		
+		private Coord getEnd(Coord start, int length, double pitch, double yaw){
+			Coord end = new Coord(start);
+			Coord offset = new Coord(
+					(int)(Math.cos(pitch) * Math.cos(yaw) * length),
+					(int)(Math.sin(yaw) * length),
+					(int)(Math.sin(pitch) * Math.cos(yaw) * length)
+					);
+			
+			end = new Coord(start);
+			end.add(offset);
+			return end;
+		}
 	}
 }
