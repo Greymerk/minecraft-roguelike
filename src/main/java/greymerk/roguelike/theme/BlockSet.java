3ec043f421749de94b67a958ac84ad44985bd21e
Samuel Waggoner <samuel.waggoner@gmail.com>
2020-03-24 21:14:59 -0700

    Prefer optional over null checks

--- src/main/java/greymerk/roguelike/theme/BlockSet.java
+++ src/main/java/greymerk/roguelike/theme/BlockSet.java
@@ -13,6 +13,8 @@ import greymerk.roguelike.worldgen.blocks.door.Door;
 import greymerk.roguelike.worldgen.blocks.door.DoorType;
 import greymerk.roguelike.worldgen.blocks.door.IDoor;
 
+import static java.util.Optional.ofNullable;
+
 public class BlockSet implements IBlockSet {
 
   private IBlockFactory floor;
@@ -60,54 +62,74 @@ public class BlockSet implements IBlockSet {
   }
 
   public BlockSet(JsonObject json, IBlockSet base) throws Exception {
+    selectWalls(json, base);
+    selectFloor(json, base);
+    selectStair(json, base);
+    selectPillar(json, base);
+    selectDoor(json, base);
+    selectLightBlock(json, base);
+    selectLiquid(json, base);
+  }
 
+  private void selectFloor(JsonObject json, IBlockSet base) throws Exception {
+    floor = json.has("floor")
+        ? BlockProvider.create(json.get("floor").getAsJsonObject())
+        : ofNullable(base)
+            .map(IBlockSet::getFloor)
+            .orElse(null);
+  }
 
-    if (json.has("walls")) {
-      walls = BlockProvider.create(json.get("walls").getAsJsonObject());
-    } else if (base != null) {
-      walls = base.getWall();
-    }
+  private void selectWalls(JsonObject json, IBlockSet base) throws Exception {
+    walls = json.has("walls")
+        ? BlockProvider.create(json.get("walls").getAsJsonObject())
+        : ofNullable(base)
+            .map(IBlockSet::getWall)
+            .orElse(null);
+  }
 
+  private void selectStair(JsonObject json, IBlockSet base) throws Exception {
+    stair = json.has("stair")
+        ? somethingAboutStairWithData(json)
+        : ofNullable(base)
+            .map(IBlockSet::getStair)
+            .orElse(null);
+  }
 
-    if (json.has("floor")) {
-      floor = BlockProvider.create(json.get("floor").getAsJsonObject());
-    } else if (base != null) {
-      floor = base.getFloor();
-    }
+  private MetaStair somethingAboutStairWithData(JsonObject json) throws Exception {
+    JsonObject stairData = json.get("stair").getAsJsonObject();
+    return stairData.has("data")
+        ? new MetaStair(new MetaBlock(stairData.get("data").getAsJsonObject()))
+        : new MetaStair(new MetaBlock(stairData));
+  }
 
-    if (json.has("stair")) {
-      JsonObject stair = json.get("stair").getAsJsonObject();
-      this.stair = stair.has("data")
-          ? new MetaStair(new MetaBlock(stair.get("data").getAsJsonObject()))
-          : new MetaStair(new MetaBlock(stair));
-    } else if (base != null) {
-      stair = base.getStair();
-    }
+  private void selectPillar(JsonObject json, IBlockSet base) throws Exception {
+    pillar = json.has("pillar")
+        ? BlockProvider.create(json.get("pillar").getAsJsonObject())
+        : ofNullable(base)
+            .map(IBlockSet::getPillar)
+            .orElse(null);
+  }
 
-    if (json.has("pillar")) {
-      pillar = BlockProvider.create(json.get("pillar").getAsJsonObject());
-    } else if (base != null) {
-      pillar = base.getPillar();
-    }
+  private void selectDoor(JsonObject json, IBlockSet base) throws Exception {
+    door = json.has("door") ? new Door(json.get("door")) : ofNullable(base)
+        .map(IBlockSet::getDoor)
+        .orElse(null);
+  }
 
-    if (json.has("door")) {
-      door = new Door(json.get("door"));
-    } else if (base != null) {
-      door = base.getDoor();
-    }
+  private void selectLightBlock(JsonObject json, IBlockSet base) throws Exception {
+    lightblock = json.has("lightblock")
+        ? BlockProvider.create(json.get("lightblock").getAsJsonObject())
+        : ofNullable(base)
+            .map(IBlockSet::getLightBlock)
+            .orElse(null);
+  }
 
-    if (json.has("lightblock")) {
-      lightblock = BlockProvider.create(json.get("lightblock").getAsJsonObject());
-    } else if (base != null) {
-      lightblock = base.getLightBlock();
-    }
-
-    if (json.has("liquid")) {
-      liquid = BlockProvider.create(json.get("liquid").getAsJsonObject());
-    } else if (base != null) {
-      liquid = base.getLiquid();
-    }
-
+  private void selectLiquid(JsonObject json, IBlockSet base) throws Exception {
+    liquid = json.has("liquid")
+        ? BlockProvider.create(json.get("liquid").getAsJsonObject())
+        : ofNullable(base)
+            .map(IBlockSet::getLiquid)
+            .orElse(null);
   }
 
   public BlockSet(IBlockSet toCopy) {
@@ -121,47 +143,47 @@ public class BlockSet implements IBlockSet {
   }
 
   public BlockSet(IBlockSet parent, IBlockSet child) {
-    walls = child.getWall() != null ? child.getWall() : parent.getWall();
-    floor = child.getFloor() != null ? child.getFloor() : parent.getFloor();
-    stair = child.getStair() != null ? child.getStair() : parent.getStair();
-    pillar = child.getPillar() != null ? child.getPillar() : parent.getPillar();
-    door = child.getDoor() != null ? child.getDoor() : parent.getDoor();
-    lightblock = child.getLightBlock() != null ? child.getLightBlock() : parent.getLightBlock();
-    liquid = child.getLiquid() != null ? child.getLiquid() : parent.getLiquid();
+    walls = ofNullable(child.getWall()).orElse(parent.getWall());
+    floor = ofNullable(child.getFloor()).orElse(parent.getFloor());
+    stair = ofNullable(child.getStair()).orElse(parent.getStair());
+    pillar = ofNullable(child.getPillar()).orElse(parent.getPillar());
+    door = ofNullable(child.getDoor()).orElse(parent.getDoor());
+    lightblock = ofNullable(child.getLightBlock()).orElse(parent.getLightBlock());
+    liquid = ofNullable(child.getLiquid()).orElse(parent.getLiquid());
   }
 
   @Override
   public IBlockFactory getWall() {
-    return walls != null ? walls : BlockType.get(BlockType.STONE_BRICK);
+    return ofNullable(walls).orElse(BlockType.get(BlockType.STONE_BRICK));
   }
 
   @Override
   public IStair getStair() {
-    return stair != null ? stair : new MetaStair(StairType.STONEBRICK);
+    return ofNullable(stair).orElse(new MetaStair(StairType.STONEBRICK));
   }
 
   @Override
   public IBlockFactory getPillar() {
-    return pillar != null ? pillar : getWall();
+    return ofNullable(pillar).orElse(getWall());
   }
 
   @Override
   public IBlockFactory getFloor() {
-    return floor != null ? floor : getWall();
+    return ofNullable(floor).orElse(getWall());
   }
 
   @Override
   public IDoor getDoor() {
-    return door != null ? door : new Door(DoorType.OAK);
+    return ofNullable(door).orElse(new Door(DoorType.OAK));
   }
 
   @Override
   public IBlockFactory getLightBlock() {
-    return lightblock != null ? lightblock : BlockType.get(BlockType.GLOWSTONE);
+    return ofNullable(lightblock).orElse(BlockType.get(BlockType.GLOWSTONE));
   }
 
   @Override
   public IBlockFactory getLiquid() {
-    return liquid != null ? liquid : BlockType.get(BlockType.WATER_FLOWING);
+    return ofNullable(liquid).orElse(BlockType.get(BlockType.WATER_FLOWING));
   }
 }
