99f9039e5fc765d29701ef3db8bc119989cbf1bb
Brian Rivas <brian.rivas64@gmail.com>
2025-04-06 15:42:01 -0700

    added cave spider nests to cobweb filter

---
+++ src/main/java/com/greymerk/roguelike/dungeon/fragment/parts/SpiderNest.java
@@ -0,0 +1,77 @@
+package com.greymerk.roguelike.dungeon.fragment.parts;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import com.greymerk.roguelike.dungeon.fragment.IFragment;
+import com.greymerk.roguelike.editor.Cardinal;
+import com.greymerk.roguelike.editor.Coord;
+import com.greymerk.roguelike.editor.Fill;
+import com.greymerk.roguelike.editor.IWorldEditor;
+import com.greymerk.roguelike.editor.MetaBlock;
+import com.greymerk.roguelike.editor.blocks.spawners.Spawner;
+import com.greymerk.roguelike.editor.boundingbox.BoundingBox;
+import com.greymerk.roguelike.editor.shapes.Line;
+import com.greymerk.roguelike.settings.ILevelSettings;
+import com.greymerk.roguelike.util.math.RandHelper;
+
+import net.minecraft.block.Blocks;
+import net.minecraft.util.math.random.Random;
+
+public class SpiderNest implements IFragment {
+
+	public static void generate(IWorldEditor editor, Random rand, ILevelSettings settings, Coord origin) {
+		new SpiderNest().generate(editor, rand, settings, origin, Cardinal.NORTH);
+	}
+	
+	@Override
+	public void generate(IWorldEditor editor, Random rand, ILevelSettings settings, Coord origin, Cardinal dir) {
+		if(!this.validLocation(editor, origin)) return;
+		
+		Spawner.generate(editor, rand, settings.getDifficulty(), origin, Spawner.CAVESPIDER);
+		List<Coord> ends = RandHelper.pickCountFrom(findSolids(editor, origin, 8), rand, 10);;
+		
+		ends.forEach(end -> {
+			getStrand(editor, origin, end).ifPresent(strand -> {
+				strand.forEach(c -> {
+					strand.fill(editor, rand, MetaBlock.of(Blocks.COBWEB), Fill.AIR);
+				});
+			});
+		});
+		
+	}
+		
+	private Optional<Line> getStrand(IWorldEditor editor, Coord start, Coord end) {
+		Line strand = Line.of(start, end);
+		
+		List<Coord> segments = strand.get();
+		
+		Collections.sort(segments, (a, b) -> {
+			return (int) (Math.round(start.distance(b)) - Math.round(start.distance(a)));
+		});
+		
+		for(Coord c : segments) {
+			if(c.equals(start)) continue;
+			
+			if(editor.isSupported(c)) return Optional.of(Line.of(start, c));
+		}
+		
+		return Optional.empty();
+	}
+	
+	private List<Coord> findSolids(IWorldEditor editor, Coord origin, int range){
+		return BoundingBox.of(origin).grow(Cardinal.all, range)
+				.get().stream().filter(c -> {
+					return editor.isSolid(c) && editor.isSupported(c);
+				}).toList();
+	}
+	
+	private boolean validLocation(IWorldEditor editor, Coord origin) {
+		for(Coord c : BoundingBox.of(origin).grow(Cardinal.all)) {
+			if(!editor.isAir(c)) return false;
+		}
+		
+		return true;
+	}
+}
--- src/main/java/com/greymerk/roguelike/filter/CobwebFilter.java
+++ src/main/java/com/greymerk/roguelike/filter/CobwebFilter.java
@@ -1,11 +1,11 @@
 package com.greymerk.roguelike.filter;
 
+import com.greymerk.roguelike.dungeon.fragment.parts.SpiderNest;
 import com.greymerk.roguelike.editor.Cardinal;
 import com.greymerk.roguelike.editor.Coord;
 import com.greymerk.roguelike.editor.IWorldEditor;
 import com.greymerk.roguelike.editor.blocks.BlockType;
 import com.greymerk.roguelike.editor.boundingbox.IBounded;
-import com.greymerk.roguelike.editor.shapes.Shape;
 import com.greymerk.roguelike.settings.ILevelSettings;
 
 import net.minecraft.util.math.random.Random;
@@ -14,20 +14,18 @@ public class CobwebFilter implements IFilter{
 
 	@Override
 	public void apply(IWorldEditor editor, Random rand, ILevelSettings settings, IBounded box) {
-		for(Coord pos : box.getShape(Shape.RECTSOLID)){
-			if(rand.nextInt(100) != 0) continue;
-			if(!editor.isAir(pos)) continue;
-			if(!validLocation(editor, pos)) continue;
-				
-			generate(editor, rand, pos, rand.nextInt(2) + 2); 
-		}
+		box.getBoundingBox().forEach(pos -> {
+			if(!editor.isAir(pos)) return;
+			if(rand.nextInt(100) != 0) return;
+			if(editor.isSolid(pos.copy().add(Cardinal.UP))) {
+				placeWeb(editor, rand, pos, rand.nextInt(2) + 2);
+			} else {
+				if(rand.nextInt(100) == 0) SpiderNest.generate(editor, rand, settings, pos);
+			}
+		});
 	}
 	
-	private boolean validLocation(IWorldEditor editor, Coord pos){
-		return !editor.isAir(pos.copy().add(Cardinal.UP));
-	}
-	
-	private void generate(IWorldEditor editor, Random rand, Coord pos, int count){
+	private void placeWeb(IWorldEditor editor, Random rand, Coord pos, int count){
 		if(!editor.isAir(pos)) return;
 		if(count <= 0) return;
 		
@@ -37,7 +35,7 @@ public class CobwebFilter implements IFilter{
 			Cardinal dir = Cardinal.values()[rand.nextInt(Cardinal.values().length)];
 			Coord cursor = pos.copy();
 			cursor.add(dir);
-			generate(editor, rand, cursor, count - 1);
+			placeWeb(editor, rand, cursor, count - 1);
 		}
 	}
 }
