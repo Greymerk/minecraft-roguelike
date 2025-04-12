ceaa0486e4284d4f05c479164197a504e5af2838
Brian Rivas <brian.rivas64@gmail.com>
2025-04-11 22:54:55 -0700

    rebalanced supply chests

--- src/main/java/com/greymerk/roguelike/treasure/loot/provider/ItemSupply.java
+++ src/main/java/com/greymerk/roguelike/treasure/loot/provider/ItemSupply.java
@@ -19,39 +19,39 @@ public class ItemSupply extends ItemBase{
 		
 		if(diff.lt(Difficulty.MEDIUM)) {
 			loot.add(new WeightedRandomLoot(Items.WHEAT_SEEDS, 1, 8, 1));
-			loot.add(new WeightedRandomLoot(Items.PUMPKIN_SEEDS, 1, 4, 1));
-			loot.add(new WeightedRandomLoot(Items.MELON_SEEDS, 1, 4, 1));
-			loot.add(new WeightedRandomLoot(Items.BEETROOT_SEEDS, 1, 4, 1));
-			loot.add(new WeightedRandomLoot(Items.WHEAT, 4, 32, 1));
-			loot.add(new WeightedRandomLoot(Items.POTATO, 2, 16, 1));
-			loot.add(new WeightedRandomLoot(Items.CARROT, 2, 16, 1));
+			loot.add(new WeightedRandomLoot(Items.PUMPKIN_SEEDS, 1));
+			loot.add(new WeightedRandomLoot(Items.MELON_SEEDS, 1));
+			loot.add(new WeightedRandomLoot(Items.BEETROOT_SEEDS, 1));
+			loot.add(new WeightedRandomLoot(Items.WHEAT, 1, 32, 1));
+			loot.add(new WeightedRandomLoot(Items.POTATO, 1, 32, 1));
+			loot.add(new WeightedRandomLoot(Items.CARROT, 1, 32, 1));
 		} else {
-			loot.add(new WeightedRandomLoot(Items.COD, 2, 32, 1));
-			loot.add(new WeightedRandomLoot(Items.SALMON, 2, 32, 1));
-			loot.add(new WeightedRandomLoot(Items.CHICKEN, 4, 32, 1));
-			loot.add(new WeightedRandomLoot(Items.PORKCHOP, 4, 32, 1));
-			loot.add(new WeightedRandomLoot(Items.MUTTON, 4, 32, 1));
-			loot.add(new WeightedRandomLoot(Items.RABBIT, 4, 32, 1));
+			loot.add(new WeightedRandomLoot(Items.COD, 1, 16, 1));
+			loot.add(new WeightedRandomLoot(Items.SALMON, 1, 16, 1));
+			loot.add(new WeightedRandomLoot(Items.CHICKEN, 1, 16, 1));
+			loot.add(new WeightedRandomLoot(Items.PORKCHOP, 1, 16, 1));
+			loot.add(new WeightedRandomLoot(Items.MUTTON, 1, 16, 1));
+			loot.add(new WeightedRandomLoot(Items.RABBIT, 1, 16, 1));
 		}
 		
-		loot.add(new WeightedRandomLoot(Items.TORCH, 8, 48, 5));
+		loot.add(new WeightedRandomLoot(Items.TORCH, 1, 32, 5));
 		loot.add(new WeightedRandomLoot(Items.RABBIT_HIDE, 1, 4, 1));
-		loot.add(new WeightedRandomLoot(Items.RABBIT_FOOT, 1, 1, 1));
+		loot.add(new WeightedRandomLoot(Items.RABBIT_FOOT, 1));
 		loot.add(new WeightedRandomLoot(Items.SWEET_BERRIES, 1, 4, 1));
-		loot.add(new WeightedRandomLoot(Items.PAPER, 4, 32, 1));
-		loot.add(new WeightedRandomLoot(Items.TURTLE_SCUTE, 1, 1, 1));
-		loot.add(new WeightedRandomLoot(Items.STRING, 8, 32, 1));
-		loot.add(new WeightedRandomLoot(Items.STICK, 8, 64, 1));
-		loot.add(new WeightedRandomLoot(Items.LEATHER, 4, 24, 1));
-		loot.add(new WeightedRandomLoot(Items.CLAY_BALL, 8, 64, 1));
-		loot.add(new WeightedRandomLoot(Items.QUARTZ, 2, 16, 1));
-		loot.add(new WeightedRandomLoot(Items.INK_SAC, 2, 12, 1));
-		loot.add(new WeightedRandomLoot(Items.GUNPOWDER, 8, 64, 1));
+		loot.add(new WeightedRandomLoot(Items.PAPER, 1, 32, 1));
+		loot.add(new WeightedRandomLoot(Items.TURTLE_SCUTE, 1));
+		loot.add(new WeightedRandomLoot(Items.STRING, 1, 16, 1));
+		loot.add(new WeightedRandomLoot(Items.STICK, 1, 16, 1));
+		loot.add(new WeightedRandomLoot(Items.LEATHER, 1, 8, 1));
+		loot.add(new WeightedRandomLoot(Items.CLAY_BALL, 1, 16, 1));
+		loot.add(new WeightedRandomLoot(Items.QUARTZ, 1, 16, 1));
+		loot.add(new WeightedRandomLoot(Items.INK_SAC, 1, 4, 1));
+		loot.add(new WeightedRandomLoot(Items.GUNPOWDER, 1, 16, 1));
 		
 		
 		WeightedRandomizer<ItemStack> saplings = new WeightedRandomizer<ItemStack>(1);
 		FlowerPot.saplings.forEach(sapling -> {
-			saplings.add(new WeightedRandomLoot(FlowerPot.getFlowerItem(sapling).getItem(), 1, 1, 1));
+			saplings.add(new WeightedRandomLoot(FlowerPot.getFlowerItem(sapling).getItem(), 1, 4, 1));
 		});
 		loot.add(saplings);
 	}
